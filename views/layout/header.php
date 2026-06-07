edd95d39552a0ff2dc9bbbfdedcd8913cc2ba72d
AltafRabbani28 <rabbanialtaf08@gmail.com>
2026-06-07 19:57:41 +0700

    Add files via upload

---
+++ views/layout/header.php
@@ -0,0 +1,6 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= htmlspecialchars($pageTitle ?? APP_NAME) ?></title>
+    <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
+</head>
