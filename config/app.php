030fa25413540558fb078e82f11d221ca8b1979a
AltafRabbani28 <rabbanialtaf08@gmail.com>
2026-06-16 20:51:59 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -3,15 +3,32 @@
 define('APP_NAME', 'StockMate');
 define('APP_VERSION', '1.0.0');
 
-$scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
-$host = $_SERVER['HTTP_HOST'] ?? 'localhost';
-$scriptDir = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'] ?? '')), '/');
-$basePath = ($scriptDir === '' || $scriptDir === '/') ? '' : $scriptDir;
+if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') {
+    $scheme = 'https';
+} else {
+    $scheme = 'http';
+}
+
+if (isset($_SERVER['HTTP_HOST'])) {
+    $host = $_SERVER['HTTP_HOST'];
+} else {
+    $host = 'localhost';
+}
+
+if (isset($_SERVER['SCRIPT_NAME'])) {
+    $scriptDir = rtrim(str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME'])), '/');
+} else {
+    $scriptDir = '';
+}
+
+if ($scriptDir === '' || $scriptDir === '/') {
+    $basePath = '';
+} else {
+    $basePath = $scriptDir;
+}
 
 define('BASE_URL', $scheme . '://' . $host . $basePath);
 
-// Timezone
 date_default_timezone_set('Asia/Jakarta');
 
-// Session
 session_start();
