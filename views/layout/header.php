630b6bb3cee588daa9ddd6827ed523ee9aa6cc8a
SandiZulian <zuliansyahsandi4@gmail.com>
2026-06-17 03:29:11 +0700

    Update header.php

--- views/layout/header.php
+++ views/layout/header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= htmlspecialchars($pageTitle ?? APP_NAME) ?></title>
+    <title><?= htmlspecialchars($pageTitle ?? (defined('APP_NAME') ? APP_NAME : 'Application')) ?></title>
     <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
 </head>
